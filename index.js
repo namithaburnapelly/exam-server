a5937621d0f405238bf814c8257e57df82bd808a
namithaburnapelly <namitha.nja@gmail.com>
2024-05-27 23:30:26 +0530

    fix try

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const cors = require('cors')
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:4200' }));
+
+
 
 mongoose.set('strictQuery', false)
 
